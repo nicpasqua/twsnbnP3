0aca22ad8ac8151eb2e86e3043c00c6657bc3587
nppasqua@buffaloedu <nppasqua@buffalo.edu>
2019-03-24 19:26:47 -0400

    refresh, welcome screen, no longer crashing on open, note and title, image can be retrieved, catagories added

--- TheAppThatWillNotBeNamed/app/src/main/java/com/example/theappthatwillnotbenamed/MainActivity.java
+++ TheAppThatWillNotBeNamed/app/src/main/java/com/example/theappthatwillnotbenamed/MainActivity.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,48 +19,64 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     Cursor todoCursor;
     ListView noteList;
     NoteAdapter adapter;
-
+    SwipeRefreshLayout swipe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        noteList = (ListView) findViewById(R.id.note_list);
-        noteList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent openNote = new Intent(MainActivity.this, NoteActivity.class);
-                openNote.putExtra("noteId", id);
-                startActivity(openNote);            }
-        });
-        if(userHasPermission()) {
-            loadNotesFromDatabase();
+        try {
+            if (userHasPermission()) {
+                loadNotesFromDatabase();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        0);
+            }
+            noteList = (ListView) findViewById(R.id.note_list);
+            noteList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent openNote = new Intent(MainActivity.this, NoteActivity.class);
+                    openNote.putExtra("noteId", id);
+                    startActivity(openNote);
+                }
+            });
+            swipe = (SwipeRefreshLayout) findViewById(R.id.swipe);
+            swipe.setOnRefreshListener(this);
+            if (todoCursor.moveToFirst())setContentView(R.layout.activity_main);
+            else setContentView(R.layout.welcome);
         }
-        else{
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    0);
+        catch(java.lang.NullPointerException e){
+            setContentView(R.layout.welcome);
         }
-        if(moveToFirst()) setContentView(R.layout.activity_main);
-
-        else setContentView(R.layout.welcome);
     }
     public void loadNotesFromDatabase() {
-        // Create a new instance of the NoteTakingDatabase
-        NoteTakingDatabase handler = new NoteTakingDatabase(getApplicationContext());
-        // Get the writable database
-        SQLiteDatabase db = handler.getWritableDatabase();
-        //Get all notes from the database
-        todoCursor = db.rawQuery("SELECT * FROM notes", null);
+        try {
+            // Create a new instance of the NoteTakingDatabase
+            NoteTakingDatabase handler = new NoteTakingDatabase(getApplicationContext());
+            // Get the writable database
+            SQLiteDatabase db = handler.getWritableDatabase();
+            //Get all notes from the database
+            todoCursor = db.rawQuery("SELECT * FROM notes", null);
 
-        // Create an instance of the NoteAdapter with our cursor
-        adapter = new NoteAdapter(this, todoCursor, 0);
+            // Create an instance of the NoteAdapter with our cursor
+            adapter = new NoteAdapter(this, todoCursor, 0);
 
-        // Set the NoteAdapter to the ListView (display all notes from DB)
-        noteList.setAdapter(adapter);
+            // Set the NoteAdapter to the ListView (display all notes from DB)
+            noteList.setAdapter(adapter);
+            setContentView(R.layout.activity_main);
+
+        }
+        catch (java.lang.NullPointerException e){
+            setContentView(R.layout.welcome);
+        }
+        catch (java.lang.RuntimeException e){
+            setContentView(R.layout.welcome);
+        }
     }
     @Override
     protected void onDestroy() {
@@ -104,5 +121,15 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        loadNotesFromDatabase();
+    }
+
+    @Override
+    public void onRefresh() {
+        loadNotesFromDatabase();
     }
 }
